5076c5465d2c4ed202cd559e727e72ca0d5dc30b
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2023-05-11 11:03:30 +0500

    ➕ Added exam models

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Department, Topic, Resource
+from .models import Department, Topic, Resource, Exam
 
 
 @admin.register(Department)
@@ -23,4 +23,13 @@ class ResourceAdmin(admin.ModelAdmin):
     list_display = ('name', 'topic', 'created_at')
     list_filter = ('created_at',)
     search_fields = ('name',)
+    ordering = ('-created_at',)
+
+
+@admin.register(Exam)
+class ExamAdmin(admin.ModelAdmin):
+    list_display = ('department', 'id', 'answer', 'created_at')
+    search_fields = ('question', 'answer_a',
+                     'answer_b', 'answer_c', 'answer_d')
+    list_filter = ('department',)
     ordering = ('-created_at',)
--- main/models.py
+++ main/models.py
@@ -64,3 +64,52 @@ class Resource(models.Model):
     class Meta:
         verbose_name = 'Resurs'
         verbose_name_plural = 'Resurslar'
+
+
+class Exam(models.Model):
+    ANSWERS = (
+        ('answer_a', 'A javob'),
+        ('answer_b', 'B javob'),
+        ('answer_c', 'C javob'),
+        ('answer_d', 'D javob')
+    )
+    department = models.ForeignKey(
+        to=Department,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name='exams_department'
+    )
+    question = models.TextField(
+        verbose_name='Savol'
+    )
+    answer_a = models.CharField(
+        verbose_name='A javob:',
+        max_length=400
+    )
+    answer_b = models.CharField(
+        verbose_name='b javob:',
+        max_length=400
+    )
+    answer_c = models.CharField(
+        verbose_name='C javob:',
+        max_length=400
+    )
+    answer_d = models.CharField(
+        verbose_name='D javob:',
+        max_length=400
+    )
+    answer = models.CharField(
+        verbose_name='Javob',
+        max_length=10,
+        choices=ANSWERS,
+        default='answer_a'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.department.name + ' || ' + str(self.id)
+
+    class Meta:
+        verbose_name = 'Imtihon'
+        verbose_name_plural = 'Imtihonlar'
